d0166d4c6dd18ce46a20a834caa28490e3967f47
faturh <fikrihabib25@gmail.com>
2025-01-19 17:32:41 +0700

    this file company profile

---
+++ app/Models/Testimonial.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Testimonial extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'thumbnail',
+        'message',
+        'project_client_id',
+    ];
+
+
+    public function projectClient()
+    {
+        return $this->belongsTo(ProjectClient::class);
+    }
+}
---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;  // Import Permission dari Spatie
+use Spatie\Permission\Models\Role;        // Import Role dari Spatie
+use App\Models\User;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // permission untuk menentukan role nya
+        $permissions = [
+            'manage statistics',
+            'manage products',
+            'manage principles',
+            'manage testimonials',
+            'manage clients',
+            'manage teams',
+            'manage abouts',
+            'manage appointments',
+            'manage hero sections',
+        ];
+
+        foreach ($permissions as $permission){
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+        // role Design Manager
+        $designManagerRole = Role::firstOrCreate(['name' => 'Designer_Manager']);
+
+        $designManagerPermissions = [
+            'manage products',
+            'manage principles',
+            'manage testimonials'
+        ];
+        
+        // Harus menggunakan syncPermissions
+        $designManagerRole->syncPermissions($designManagerPermissions);
+
+        // role super admin
+        $superAdminRole = Role::firstOrCreate(['name' => 'super-admin']);
+
+        $user = User::create([
+            'name' => 'Voycomp',
+            'email' => 'superadmin@gmail.com',
+            'password' => bcrypt('palayam'),
+        ]);
+
+        $user->assignRole($superAdminRole);
+    }
+}
