01b0b2462fe4e546010e2a7ebb07e1cecb1ff140
faturh <fikrihabib25@gmail.com>
2025-01-20 20:53:20 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\CompanyAboutController;
+use App\Http\Controllers\CompanyStatisticController;
+use App\Http\Controllers\FrontController;
 use App\Http\Controllers\HeroSectionController;
 use App\Http\Controllers\OurPrincipleController;
 use App\Http\Controllers\OurTeamController;
@@ -11,17 +13,28 @@ use App\Http\Controllers\ProjectClientController;
 use App\Http\Controllers\TestimonialController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FrontController::class, 'index'])->name('front.index');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'upadate'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Route::put('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::post('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
 
-    Route::prefix('admin')->name('admin')->group(function () {
+    Route::prefix('admin')->name('admin.')->group(function () {
+        
+        Route::middleware('can:manage statistics')->group(function () {
+            Route::resource('statistics', CompanyStatisticController::class);
+        });
 
         Route::middleware('can:manage principles')->group(function () {
             Route::resource('principles', OurPrincipleController::class);
@@ -53,7 +66,9 @@ Route::middleware('auth')->group(function () {
         
         Route::middleware('can:manage hero sections')->group(function () {
             Route::resource('hero_sections', HeroSectionController::class);
-        });        
+        });
+
+        
 
     });
 
