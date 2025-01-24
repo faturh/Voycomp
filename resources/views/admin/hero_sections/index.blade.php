3e013ebd27f209e6a8d0987096a90794852b3055
faturh <fikrihabib25@gmail.com>
2025-01-24 14:21:45 +0700

    update fort hero_section

--- resources/views/admin/hero_sections/index.blade.php
+++ resources/views/admin/hero_sections/index.blade.php
@@ -13,7 +13,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10 flex flex-col gap-y-5">
- 
+                @forelse($hero_sections as $hero_section)
                 <div class="item-card flex flex-row justify-between items-center">
                     <div class="flex flex-row items-center gap-x-3">
                         
